d0243faa67b30e788f6199459b38ba39d4a9c58f
Seth Wagener <sethwagener@gmail.com>
2020-11-11 21:39:27 +1300

    Initial Commit

---
+++ RPG test/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace RPG_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTarget = new System.Windows.Forms.Label();
+            this.tmrTarget = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblEnemy2 = new System.Windows.Forms.Label();
+            this.lblEnemy3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTarget
+            // 
+            this.lblTarget.AutoSize = true;
+            this.lblTarget.Location = new System.Drawing.Point(157, 199);
+            this.lblTarget.Name = "lblTarget";
+            this.lblTarget.Size = new System.Drawing.Size(0, 17);
+            this.lblTarget.TabIndex = 0;
+            // 
+            // tmrTarget
+            // 
+            this.tmrTarget.Enabled = true;
+            this.tmrTarget.Tick += new System.EventHandler(this.tmrTarget_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 1;
+            // 
+            // lblEnemy2
+            // 
+            this.lblEnemy2.AutoSize = true;
+            this.lblEnemy2.Location = new System.Drawing.Point(284, 199);
+            this.lblEnemy2.Name = "lblEnemy2";
+            this.lblEnemy2.Size = new System.Drawing.Size(0, 17);
+            this.lblEnemy2.TabIndex = 2;
+            // 
+            // lblEnemy3
+            // 
+            this.lblEnemy3.AutoSize = true;
+            this.lblEnemy3.Location = new System.Drawing.Point(349, 199);
+            this.lblEnemy3.Name = "lblEnemy3";
+            this.lblEnemy3.Size = new System.Drawing.Size(0, 17);
+            this.lblEnemy3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEnemy3);
+            this.Controls.Add(this.lblEnemy2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTarget);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTarget;
+        private System.Windows.Forms.Timer tmrTarget;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblEnemy2;
+        private System.Windows.Forms.Label lblEnemy3;
+    }
+}
+
---
+++ RPG test/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPG_test
+{
+    public partial class Form1 : Form
+    {
+        int HP;
+        int MP;
+        int ATK=8;
+        int SPD;
+        int DEF;
+        int HP1= 100;
+        int MP1;
+        int ATK1;
+        int SPD1;
+        int DEF1=4;
+        int HP2 =100;
+        int MP2;
+        int ATK2;
+        int SPD2;
+        int DEF2 = 4;
+        int HP3= 100;
+        int MP3;
+        int ATK3;
+        int SPD3;
+        int DEF3= 4;
+        bool Target1;
+        bool Target2;
+        bool Target3;
+        int Target;
+        bool Targeting;
+        bool Damage;
+        int DMG;
+        int x;
+        public Form1()
+        {
+            InitializeComponent();
+            Target = 1;
+            Targeting = true;
+            label1.Text = "HP:" + HP1;
+            lblEnemy2.Text = "HP" + HP2;
+            lblEnemy3.Text = "HP:" + HP3;
+        }
+        //Methods start here
+        public void Attack()
+        {
+            if (Damage == true); 
+            {
+                if (Target == 1)
+                {
+                    if (HP1 > 0)
+                    {
+                        DMG = ATK - DEF1;
+                        HP1 -= DMG;
+                        label1.Text = "HP:" + HP1;
+                    }
+                }
+            }
+            if (Damage == true)
+            {
+                if (Target == 2)
+                {
+                    if (HP2 > 0)
+                    {
+                        DMG = ATK - DEF2;
+                        HP2 -= DMG;
+                        lblEnemy2.Text = "HP:" + HP2;
+                    }
+                }
+            }
+            if (Damage == true) 
+            {
+                if (Target == 3)
+                {
+                    if (HP3 > 0)
+                    {
+                        DMG = ATK - DEF3;
+                        HP3 -= DMG;
+                        lblEnemy3.Text = "HP:" + HP3;
+                    }
+                }
+            }
+        }
+        
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Targeting debug
+            if (e.KeyData == Keys.W)
+            {
+                Console.WriteLine(Target);
+            }
+            //Targeting system
+            if (e.KeyData == Keys.Left)
+            {
+                if (Targeting == true & Target >= 2)
+                {
+                    Target = Target - 1;
+                }
+                else
+                {
+                    if (Targeting == true & Target == 1)
+                    {
+                        Target = 3;
+                    }
+                }
+            }
+            if (e.KeyData == Keys.Right)
+            {
+                if (Targeting == true & Target <= 2)
+                {
+                    Target = Target + 1;
+                }
+                else
+                {
+                    if (Targeting == true & Target == 3)
+                    {
+                        Target = 1;
+                    }
+                }
+            }
+            if (e.KeyData == Keys.Space)
+            {
+                Damage = false;
+             
+            }
+        }
+
+        private void tmrTarget_Tick(object sender, EventArgs e)
+        {
+            lblTarget.Text = "" + Target;
+            if (HP1 <= 0)
+            {
+                HP1 = 0; 
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Space)
+            {
+                Damage = true;
+                Attack();
+            }
+        }
+    }
+}
